22643eda6aa0ab49b9f8918565522a2736f15aed
Kumar Abhishek <kumarabhishek@Kumars-MacBook-Air.local>
2023-06-29 10:21:54 +0530

    Game reset logic

--- tictactoe/src/App.vue
+++ tictactoe/src/App.vue
@@ -1,37 +1,38 @@
 <template>
-<main 
-  class="d-flex flex-column justify-content-center align-items-center bg-dark">
-  <div class="container">
-    <div class="row justify-content-center mt-3">
-      <div class="col col-6">
-        <div class="text-center">
-          <div v-if="!winMessage">
-            <h1 class="text-info" v-show="isCross">
-              Cross Turn
-            </h1>
-            <h1 class="text-info" v-show="!isCross">
-              Circle Turn
-            </h1> 
+  <main class="d-flex flex-column justify-content-center align-items-center bg-dark">
+    <div class="container">
+      <div class="row justify-content-center mt-3">
+        <div class="col col-6">
+          <div class="text-center">
+            <div v-if="!winMessage">
+              <h1 class="text-info" v-show="isCross">
+                Cross Turn
+              </h1>
+              <h1 class="text-info" v-show="!isCross">
+                Circle Turn
+              </h1>
+            </div>
+            <div v-else>
+              <h1 class="text-warning">
+                {{ winMessage.toUpperCase() }}
+              </h1>
+            </div>
           </div>
-          <div v-else>
-            <h1 class="text-warning">
-              {{ winMessage.toUpperCase() }}
-            </h1>
+          <div class="grid">
+            <div v-for="(item, i) in itemArray" :key="i" @click="handleClick(i)"
+              class="card card-body box justify-content-center align-items-center bg-light">
+              <iconComponent :iconName="item" />
+            </div>
           </div>
-        </div>
-        <div class="grid">
-          <div 
-            v-for="(item, i) in itemArray"
-            :key="i"
-            @click="handleClick(i)"
-            class="card card-body box justify-content-center align-items-center bg-light">
-            <iconComponent :iconName="item"/>
+          <div class="text text-center mt-3">
+            <button class="btn btn-danger btn-block pl-5 pr-5" @click="reloadGame">
+              RESET
+            </button>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</main>
+  </main>
 </template>
 
 <script>
@@ -51,8 +52,8 @@ export default {
     }
   },
   watch: {
-    winMessage: function(message) {
-      if(message) {
+    winMessage: function (message) {
+      if (message) {
         this.showDialog();
       }
     }
@@ -61,11 +62,11 @@ export default {
     showDialog() {
       alert(`${this.winMessage}`);
     },
-    handleClick(itemNumber){
-      if(this.winMessage){
+    handleClick(itemNumber) {
+      if (this.winMessage) {
         alert('This game is over');
       }
-      if(this.itemArray[itemNumber] === 'empty'){
+      if (this.itemArray[itemNumber] === 'empty') {
         this.itemArray[itemNumber] = this.isCross ? 'cross' : 'circle';
         this.isCross = !this.isCross;
       } else {
@@ -74,29 +75,29 @@ export default {
       this.checkIsWinner();
     },
     checkIsWinner() {
-      if(this.itemArray[0] !== 'empty' && this.itemArray[0] === this.itemArray[1] && this.itemArray[0] === this.itemArray[2]) {
+      if (this.itemArray[0] !== 'empty' && this.itemArray[0] === this.itemArray[1] && this.itemArray[0] === this.itemArray[2]) {
         this.winMessage = `${this.itemArray[0]} won`;
       }
-      else if(this.itemArray[3] !== 'empty' && this.itemArray[3] === this.itemArray[4] && this.itemArray[3] === this.itemArray[5]) {
-        this.winMessage = `${this.itemArray[3]} won`; 
+      else if (this.itemArray[3] !== 'empty' && this.itemArray[3] === this.itemArray[4] && this.itemArray[3] === this.itemArray[5]) {
+        this.winMessage = `${this.itemArray[3]} won`;
       }
-      else if(this.itemArray[6] !== 'empty' && this.itemArray[6] === this.itemArray[7] && this.itemArray[6] === this.itemArray[8]) {
+      else if (this.itemArray[6] !== 'empty' && this.itemArray[6] === this.itemArray[7] && this.itemArray[6] === this.itemArray[8]) {
         this.winMessage = `${this.itemArray[6]} won`;
       }
-      else if(this.itemArray[0] !== 'empty' && this.itemArray[0] === this.itemArray[3] && this.itemArray[0] === this.itemArray[6]) {
+      else if (this.itemArray[0] !== 'empty' && this.itemArray[0] === this.itemArray[3] && this.itemArray[0] === this.itemArray[6]) {
         this.winMessage = `${this.itemArray[0]} won`;
       }
-      else if(this.itemArray[1] !== 'empty' && this.itemArray[1] === this.itemArray[4] && this.itemArray[1] === this.itemArray[7]) {
+      else if (this.itemArray[1] !== 'empty' && this.itemArray[1] === this.itemArray[4] && this.itemArray[1] === this.itemArray[7]) {
         this.winMessage = `${this.itemArray[1]} won`;
       }
-      else if(this.itemArray[2] !== 'empty' && this.itemArray[2] === this.itemArray[5] && this.itemArray[2] === this.itemArray[8]) {
+      else if (this.itemArray[2] !== 'empty' && this.itemArray[2] === this.itemArray[5] && this.itemArray[2] === this.itemArray[8]) {
         this.winMessage = `${this.itemArray[2]} won`;
       }
-      else if(this.itemArray[0] !== 'empty' && this.itemArray[0] === this.itemArray[4] && this.itemArray[0] === this.itemArray[8]) {
+      else if (this.itemArray[0] !== 'empty' && this.itemArray[0] === this.itemArray[4] && this.itemArray[0] === this.itemArray[8]) {
         this.winMessage = `${this.itemArray[0]} won`;
       }
-      else if(this.itemArray[2] !== 'empty' && this.itemArray[2] === this.itemArray[4] && this.itemArray[2] === this.itemArray[6]) {
-        this.winMessage = `${this.itemArray[2]} won`; 
+      else if (this.itemArray[2] !== 'empty' && this.itemArray[2] === this.itemArray[4] && this.itemArray[2] === this.itemArray[6]) {
+        this.winMessage = `${this.itemArray[2]} won`;
       }
     },
     reloadGame() {
@@ -112,6 +113,7 @@ export default {
 main {
   height: 100vh;
 }
+
 .grid {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
